0a8dabc7a3a1f6c51cd5046e0f9a2e25ff352b38
emzan <zanettiemilio@hotmail.com>
2018-02-22 00:34:32 +0800

    Initial Commit

---
+++ C_ProgrammingLessons/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C_ProgrammingLessons
+//
+//  Created by George Martinson on 22/02/2018.
+//  Copyright © 2018 kuangchi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
