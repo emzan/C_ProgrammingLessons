e5483b37e5849b949d6d9f35ecd5e5e6b6721062
emzan <zanettiemilio@hotmail.com>
2018-02-24 14:10:13 +0800

    arraysorting1_3

---
+++ C_ProgrammingLessons/14_01_arraysort3.c
@@ -0,0 +1,48 @@
+//
+//  14_01_arraysort3.c
+//  C_ProgrammingLessons
+//
+//  Created by George Martinson on 24/02/2018.
+//  Copyright © 2018 kuangchi. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define SIZE 1000
+
+int compare(const void *a, const void *b);
+
+int main()
+{
+    int numbers[SIZE];
+    int x;
+    
+    /* Sort the array */
+    srand((unsigned)time(NULL));
+    for (x = 0; x < SIZE; x++)
+        numbers[x] = rand() % 100000000 + 1;
+    
+    /* Display the sorted array */
+    puts("Sorted array:");
+    for (x = 0; x < SIZE; x++)
+        printf(" %3d",numbers[x]);
+    putchar('\n');
+    
+    /* Sort the array. The following is a quicksort method, more efficient, prototyped in the sdtlib */
+    qsort(numbers,SIZE,sizeof(int),compare);
+    
+    /* Display the sorted array */
+    puts("Sorted array:");
+    for (x = 0; x < SIZE; x++)
+        printf(" %3d",numbers[x]);
+    putchar('\n');
+    
+    return (0);
+}
+
+int compare(const void *a, const void *b)
+{
+    return( *(int *)a - *(int *)b);
+}
