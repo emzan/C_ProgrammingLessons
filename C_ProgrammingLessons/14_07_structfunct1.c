3f0bec063f1d844dd381197e60de16b5e20649ad
emzan <zanettiemilio@hotmail.com>
2018-02-24 17:06:59 +0800

    structuresPop&rock&roll

---
+++ C_ProgrammingLessons/14_07_structfunct1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+struct person{
+    char name[32];
+    int age;
+    float iq;
+};
+
+void showStruct(struct person p);
+
+int main()
+{
+    struct person author;
+    
+    strcpy(author.name, "Tsai Minglong");
+    author.age = 54;
+    author.iq = 287.5;
+    
+    showStruct(author);
+    
+    return(0);
+}
+
+void showStruct(struct person p)
+{
+    printf("Author %s is %d years old\n",
+           p.name,
+           p.age);
+    printf("%s has an IQ of %.1f\n",
+           p.name,
+           p.iq);
+}
